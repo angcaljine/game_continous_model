c7c15552dd2b29b886cf1d44d77f38954f50a468
angcaljine <123051262+angcaljine@users.noreply.github.com>
2023-02-14 14:43:14 +0900

    Add files via upload

--- SimReputationDynamics_continueL3_rep_exee_Retry.m
+++ SimReputationDynamics_continueL3_rep_exee_Retry.m
@@ -11,8 +11,7 @@ function[TIME,MEndAveM]=SimReputationDynamics_continueL3_rep_exee_Retry(N,p,q,nI
 % nIt, number of iterations of the indirect reciprocity game.
 START=datetime;
 %% Setting up the objects
-for W=0:10
-w=0.01*W;
+w=0.015;
 for D=1:2
 if D==1
     DD=2;
@@ -34,10 +33,12 @@ Do=randi(N); % Selecting a donor
 Re=Do; while Re==Do, Re=randi(N); end % Selecting a different receiver
 stD=MC(Do,Do); stR=MC(Do,Re); % Defining the players¡¯ standings
 if Do<=N*p  % If donor is mutant
-ActRule=w*stD*(1-stR)+stR;
+%ActRule=(1-u)*(w*stD+stR-(d+w)*stD*stR)+u*(w*stD+stR-(d+w)*stD*stR);
+ActRule=w*stD+stR-w*stD*stR;
 %ActRule=1+(-1+w)*stD+(1-w)*stD*stR;
 else
-ActRule=w*stD*(1-stR)+stR;
+%ActRule=(1-u)*(w*stD*(1-stR)+stR)+u*(w*stD*(1-stR)+stR);
+ActRule=w*stD+stR-w*stD*stR;
 %ActRule=1+(-1+w)*stD+(1-w)*stD*stR;
 end
 cp=ActRule; % cp=1 if donor cooperates
@@ -47,16 +48,29 @@ if Obs==Do || Obs==Re || rand(1)<q % If individual observes interaction
 stD=MC(Obs,Do); stR=MC(Obs,Re); % Retrieving the players¡¯ standings
 if Obs<=N*p
 %AssRule=cp+w*stD*stR-(d+w)*stD*cp*stR; %IS, Ayz=0
-AssRule=- stR+w*stD*stR+cp*stR+(-d-w)*stD*cp*stR+1;%L3 Ayz=1
+%AssRule=-stR+w*stD*stR+cp*stR+(-d-w)*stD*cp*stR+1;%L3 Ayz=1
 %AssRule=stD+cp-stD*cp+(w-1)*stD*stR+(1-d-w)*stD*cp*stR; %L1
 %AssRule=cp*(stD-1)-stR+w*stD*stR+2*cp*stR+(-1-d-w)*stD*cp*stR+1; %L4
 %AssRule=stD+(-1+w)*stD*stR+cp*stR+(-d-w)*stD*cp*stR; %L7
+%AssRule=stD+cp-2*stD*cp+(w-1)*stD*stR+(2-w-d)*stD*cp*stR; %L2
+%AssRule=-stD*cp-stR+w*stD*stR+cp*stR+(1-d-w)*stD*cp*stR+1; %L5
+%AssRule=-cp-stR+w*stD*stR+2*cp*stR+(-d-w)*stD*cp*stR+1; %L6
+AssRule=stD-stD*cp+(-1+w)*stD*stR+cp*stR+(1-d-w)*stD*cp*stR; %L8
+%AssRule=(1-u)*(-stR+w*stD*stR+cp*stR-(d+w)*stD*cp*stR+1)+u*(cp+w*stD*stR-(d+w)*stD*cp*stR); %(1-u)*SS+u*IS
 else
 %AssRule=cp+w*stD*stR*(1-cp); %Ayz=0
-AssRule=-stR+w*stD*stR+cp*stR-w*stD*cp*stR+1; %Ayz=1
+%AssRule=-stR+w*stD*stR+cp*stR-w*stD*cp*stR+1; %Ayz=1
 %AssRule=stD+cp-stD*cp+(w-1)*stD*stR+(1-w)*stD*cp*stR; %L1
 %AssRule=cp*(stD-1)-stR+w*stD*stR+2*cp*stR+(-1-w)*stD*cp*stR+1; %L4
 %AssRule=stD+(-1+w)*stD*stR+cp*stR-w*stD*cp*stR; %L7
+%AssRule=stD+cp-2*stD*cp+(w-1)*stD*stR+(2-w)*stD*cp*stR; %L2
+%AssRule=-stD*cp-stR+w*stD*stR+cp*stR+(1-w)*stD*cp*stR+1; %L5
+%AssRule=-cp-stR+w*stD*stR+2*cp*stR-w*stD*cp*stR+1; %L6
+AssRule=stD-stD*cp+(-1+w)*stD*stR+cp*stR+(1-w)*stD*cp*stR; %L8
+%AssRule=(1-u)*(-stR+w*stD*stR+cp*stR-w*stD*cp*stR+1)+u*(cp+w*stD*stR*(1-cp)); %(1-u)*SS+u*IS
+end
+if AssRule > 1
+AssRule = 1; 
 end
 MC(Obs,Do)=AssRule;
 end
@@ -75,9 +89,7 @@ MEnd11=sum(sum(MEnd(N*p+1:N,N*p+1:N)))/(N^2*(1-p)^2);
 MEndAve(r,:)=[MEnd00 MEnd01 MEnd10 MEnd11];
 end
 %MEndAveM(10*(W-1)+D,:)=[w,d,mean(MEndAve)];
-MEndAveM(2*W+D,:)=[w,d,mean(MEndAve),std(MEndAve)/sqrt(l)];
+MEndAveM(D,:)=[d,mean(MEndAve),std(MEndAve)/sqrt(l)];
 end
-end
-FIN=datetime
+FIN=datetime;
 TIME=FIN-START;
-end
